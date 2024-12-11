f1e7138a03b0b8a7eb155ea462f43cb86d0086f0
hiroMurakami <hirohiro.hiro1158@gmail.com>
2024-12-11 16:38:43 +0900

    DB接続処理を追加

--- GitSample/Form1.cs
+++ GitSample/Form1.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using MySqlConnector;
+using System;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GitSample
@@ -19,9 +15,43 @@ namespace GitSample
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Test");
-            MessageBox.Show("Test");
+            string connectionString = "server=192.168.180.239; database=chat_board; userid=murakami; password=hirokazu;";
 
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string selectSQL = "SELECT * FROM comments WHERE comment_id = 8";
+                    using (MySqlCommand command = new MySqlCommand(selectSQL, connection))
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+
+                        // データを確認する
+                        if (dataTable.Rows.Count > 0)
+                        {
+                            string output = string.Join(Environment.NewLine,
+                                dataTable.Rows.Cast<DataRow>().Select(row => string.Join(", ", row.ItemArray)));
+                            MessageBox.Show(output, "検索結果");
+                        }
+                        else
+                        {
+                            MessageBox.Show("データが見つかりませんでした。", "情報");
+                        }
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show($"データベースエラー: {ex.Message}", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"一般的なエラー: {ex.Message}\n\n{ex.StackTrace}", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
